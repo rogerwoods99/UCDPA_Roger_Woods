896491566a085ad408161615d979709716a1c0cd
rwoods <rogerwoods99@hotmail.com>
2021-03-11 10:32:51 +0000

    Initial commit

---
+++ Shapefile Test.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import shapefile
+#import geopandas as gpd
+
+import numpy as np
+from shapely import wkt
+from shapely.geometry import Point # Point class
+from shapely.geometry import shape # shape() is a function to convert geo objects through the interface
+import pandas as pd
+
+# get Met data
+filename="MetEireann 200.txt"
+grDat=pd.read_csv(filename)
+grDat["County"]=""
+
+#print(grDat)
+sf = shapefile.Reader("shapefiles/test/gadm36_IRL_lessPtsIRL_region")  # this only has one shape to search through
+
+shapes=sf.shapes()
+
+#print(len(shapes))
+print(shapes[25])   # 342 shapes
+
+point_to_check = (214000,278000) # an x,y tuple
+all_shapes = sf.shapes() # get all the polygons
+all_records = sf.records()
+
+# search through each shape to find the one the point is in
+for i in range(200):
+    point_to_check = (grDat.loc[i,"east"], grDat.loc[i,"north"])
+    print(point_to_check)
+    for d in range(25):
+        if Point(point_to_check).within(shape(shapes[d])):  # make a point and see if it's in the polygon
+            name = all_records[d][3]  # get the second field of the corresponding record
+            print("The point is in " + str(name))
+
+#for i in len(all_shapes):
+#    boundary = all_shapes[i] # get a boundary polygon
+#    if Point(point_to_check).within(shape(boundary)): # make a point and see if it's in the polygon
+#       name = all_records[i][2] # get the second field of the corresponding record
+#       print("The point is in" + str(name))
+
+
+plt.figure(figsize=(9, 11), dpi=80)
+for shape in sf.shapeRecords():
+    x = [i[0] for i in shape.shape.points[:]]
+    y = [i[1] for i in shape.shape.points[:]]
+    #print(x)
+    plt.plot(x,y)
+#plt.show()
+
+#poly = wkt.loads(shapes[0])
+#poly = wkt.loads('POLYGON((30 10, 40 40, 20 40, 10 20, 30 10))')
+#pt = wkt.loads('POINT(20 20)')
+#print(poly.distance(pt))  # 0.0
+#print(poly.boundary.distance(pt))
